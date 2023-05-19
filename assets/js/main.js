67ff32abb5f2d8925e34afd2c112cc80fc662ffb
Davide Grassitelli <dagrassitelli@gmail.com>
2023-05-19 22:24:15 +0200

    structure done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,100 @@
-function saluta(params) {
-  console.log(`Hello, ${params}`);
+onload = () => {
+  const load = document.getElementById('load');
+
+  setTimeout(() => {
+    load.style.display = 'none';
+  }, 1000);
+};
+
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
 }
 
-saluta('Davide ');
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+const navLink = document.querySelectorAll('.nav_link');
+
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+};
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add('scroll-header')
+    : header.classList.remove('scroll-header');
+};
+window.addEventListener('scroll', scrollHeader);
+
+/* mixitup filter  */
+let mixerProducts = mixitup('.products_content', {
+  selectors: {
+    target: '.products_card',
+  },
+  animation: {
+    duration: 300,
+  },
+});
+// default filter products
+mixerProducts.filter('.red');
+// link active products
+const linkProducts = document.querySelectorAll('.products_item');
+
+function activeProducts() {
+  linkProducts.forEach((l) => l.classList.remove('active-product'));
+  this.classList.add('active-product');
+}
+
+linkProducts.forEach((l) => l.addEventListener('click', activeProducts));
+
+/*=============== SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector(
+        '.nav_menu a[href*=' + sectionId + ']'
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+window.addEventListener('scroll', scrollActive);
